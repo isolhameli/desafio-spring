664304f67c8a63234c9b42eb1e8c8f1a3c9e51e5
Israel Solha <israel.solha@mercadolivre.com>
2021-06-07 12:45:31 -0300

    Swagger implementado

--- src/main/java/com/mercadolibre/desafiospring/controllers/ProductController.java
+++ src/main/java/com/mercadolibre/desafiospring/controllers/ProductController.java
@@ -11,6 +11,8 @@ import com.mercadolibre.desafiospring.responses.products.PromotionalPostListResp
 import com.mercadolibre.desafiospring.services.PostService;
 import com.mercadolibre.desafiospring.services.ProductService;
 import com.mercadolibre.desafiospring.views.PostView;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,6 +20,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value ="/products")
+@Api(tags = "Products")
 public class ProductController {
 
     private final ProductService productService;
@@ -28,6 +31,7 @@ public class ProductController {
         this.postService = postService;
     }
 
+    @ApiOperation(value = "US 0005 - Register a new non promotional Post for a given Seller")
     @PostMapping(value="/newpost") // US 0005
     @JsonView(PostView.Detailed.class)
     ResponseEntity<PostResponse> create(@RequestBody @Valid PostRequest postRequest){
@@ -35,6 +39,7 @@ public class ProductController {
         return ResponseEntity.ok(post);
     }
 
+    @ApiOperation(value = "US 0010 - Register a new promotional Post for a given Seller")
     @PostMapping(value="/newpromopost") // US 0010
     @JsonView(PostView.PromotionalDetailed.class)
     ResponseEntity<PostResponse> createPromotional(@RequestBody @Valid PromotionalPostRequest postRequest){
@@ -42,6 +47,7 @@ public class ProductController {
         return ResponseEntity.ok(post);
     }
 
+    @ApiOperation(value = "US 0006 and US 0009 - List all Posts by Sellers followed by a Client (userId) posted in the last 14 days")
     @GetMapping(value="/followed/{userId}/list") // US 0006 and US 0009
     @JsonView(PostView.Simple.class)
     ResponseEntity<FollowedPostsResponse> followedProducts(@PathVariable Integer userId, @RequestParam(defaultValue = "desc") String order){
@@ -49,12 +55,14 @@ public class ProductController {
         return ResponseEntity.ok(postResponseList);
     }
 
+    @ApiOperation(value = "US 0011 - Count all Promotional Posts by a Seller")
     @GetMapping(value = "/{userId}/countPromo") // US 0011
     ResponseEntity<PromotionalPostCountResponse> promotionalPostCount(@PathVariable Integer userId){
         PromotionalPostCountResponse promotionalPostCount = postService.getPromotionalPostCount(userId);
         return ResponseEntity.ok(promotionalPostCount);
     }
 
+    @ApiOperation(value = "US 0012 - Lists all Promotional Posts by a Seller")
     @GetMapping(value = "/{userId}/list") // US 0012
     @JsonView(PostView.PromotionalSimple.class)
     ResponseEntity<PromotionalPostListResponse> promotionalPostList(@PathVariable Integer userId){
--- src/main/java/com/mercadolibre/desafiospring/controllers/UserController.java
+++ src/main/java/com/mercadolibre/desafiospring/controllers/UserController.java
@@ -7,6 +7,8 @@ import com.mercadolibre.desafiospring.responses.users.FollowList;
 import com.mercadolibre.desafiospring.responses.users.SellerFollowersCount;
 import com.mercadolibre.desafiospring.responses.users.UserCreateResponse;
 import com.mercadolibre.desafiospring.services.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +16,7 @@ import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value ="/users")
+@Api(tags = "Users")
 public class UserController {
 
     private final UserService userService;
@@ -22,17 +25,20 @@ public class UserController {
         this.userService = userService;
     }
 
+    @ApiOperation(value = "US 0000 - Creates a new Client")
     @PostMapping(value="/create") // US 0000 (must create User before following or posting)
     ResponseEntity<UserCreateResponse> create(@RequestBody @Valid UserRequest userRequest){
         return ResponseEntity.ok(userService.create(userRequest));
     }
 
+    @ApiOperation(value = "US 0001 - Allows a Client to follow a Seller")
     @PostMapping(value = "/{userId}/follow/{userIdToFollow}") // US 0001
     ResponseEntity follow(@PathVariable Integer userId, @PathVariable Integer userIdToFollow){
         userService.follow(userId, userIdToFollow);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "US 0002 - Count of followers of a Seller")
     @GetMapping(value = "/{userId}/followers/count") // US 0002
     ResponseEntity<SellerFollowersCount> getFollowersCount(@PathVariable Integer userId) {
 
@@ -41,18 +47,21 @@ public class UserController {
         return ResponseEntity.ok(followerCount);
     }
 
+    @ApiOperation(value = "US 0003 and US0008 - List of Sellers followed by a Client")
     @GetMapping(value = "/{userId}/followers/list") // US 0003 and US 0008
     ResponseEntity<FollowList> getFollowedList(@PathVariable Integer userId, @RequestParam(defaultValue = "name_asc") String order){
         FollowList followList = userService.getFollowers(userId, order);
         return ResponseEntity.ok(followList);
     }
 
+    @ApiOperation(value = "US 0004 and US0008 - List of Clients following a Seller")
     @GetMapping(value = "/{userId}/followed/list") // US 0004 US 0008
     ResponseEntity<FollowList> getFollowersList(@PathVariable Integer userId, @RequestParam(defaultValue = "name_asc") String order){
         FollowList followList = userService.getFollowed(userId, order);
         return ResponseEntity.ok(followList);
     }
 
+    @ApiOperation(value = "US 0007 - Allows a Client to unfollow a Seller")
     @PostMapping(value = "/{userId}/unfollow/{userIdToUnfollow}") // US 0007
     ResponseEntity<FollowList> getFollowedList(@PathVariable Integer userId, @PathVariable Integer userIdToUnfollow){
         userService.unfollow(userId, userIdToUnfollow);
